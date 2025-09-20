319eaa8870c893630f45ddce14973ee9428fb5cb
Aakashdeep <aakashdeepk9@gmail.com>
2025-09-20 18:09:29 +0530

    Add files via upload
    Create a Personalized Greeting

---
+++ Task2.py
@@ -0,0 +1,8 @@
+# Task 2: Create a Personalized Greeting
+# Get input from user
+firstname=input("Aakashdeep: ")
+lastname=input("Kanojia: ")
+# Concatenate names into a full name
+Full_name=firstname +""+ lastname
+# Display the greeting message
+print(f"\Hello, {firstname}! Welcome to the world of python!")
